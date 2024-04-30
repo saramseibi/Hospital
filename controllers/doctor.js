3240c66356cd39f609012922ba0c27dac39a8b77
saramseibi <sarramseibi2@gmail.com>
2024-04-30 21:59:57 +0100

    add doc page

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -1,10 +1,16 @@
+//require('dotenv').config(); 
 const mysql = require('mysql2');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const express = require('express')
 const path = require('path');
+const { Web3 } = require('web3');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs').promises;
+const multer = require('multer');
 const router = express.Router();
+const { create } = require('ipfs-http-client');
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -59,62 +65,8 @@ const signin = async (req, res, next) => {
     }
 
 };
-// edit profile 
 
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '..', 'public', 'uploads'));
-    },
-    filename: function (req, file, cb) {
-        console.log(file);
-        cb(null, file.originalname);
-
-    }
-});
-
-const upload = multer({ storage: storage });
-
-router.post('/doctoreditprofile', upload.single('fileToUpload'), async (req, res) => {
-    const { name, password, cpassword } = req.body;
-    const ID = req.session.doctorid;
-
-    const file = req.file;
-    console.log(req.file)
-
-    if (password !== cpassword) {
-        return res.render('doctoreditprofile', {
-            message: "Passwords do not match."
-        });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 8);
-
-    try {
-        const userProfileImagePath = path.join('/uploads', req.file.originalname);
-        let filepath = file ? file.path : null;
-        if (filepath) {
-            await db.promise().query('UPDATE doctor SET name = ?, password = ?, image = ? WHERE ID = ?', [name, hashedPassword, userProfileImagePath, ID]);
-
-        }
-
-        const [results] = await db.promise().query('SELECT name, image FROM doctor WHERE ID = ?', [ID]);
-        const { name: doctorname, image: doctorProfileImage } = results[0];
-
-
-        return res.render('doctoreditprofile', {
-            message: "Profile updated successfully!",
-            doctorname: doctorname,
-            doctorProfileImage: doctorProfileImage
-        });
-
-    } catch (err) {
-        console.error(err);
-        return res.render('doctoreditprofile', { message: "An error occurred" });
-    }
-
-});
 // forget password 
 const forget = async (req, res) => {
     const email = req.body.email;
@@ -204,9 +156,9 @@ const reset = async (req, res) => {
 const search = (req, res) => {
     console.log('Query parameters:', req.query);
     const name = req.query.name || '';
-    const doctorname=req.session.doctorname;
+    const doctorname = req.session.doctorname;
     const query = 'SELECT * FROM appointments WHERE LOWER(name) LIKE LOWER(?) AND doctor_name = ?';
-    const values = [`%${name}%`,doctorname];
+    const values = [`%${name}%`, doctorname];
 
     //console.log('Executing query:', query);
     //console.log('With values:', values);
@@ -225,9 +177,9 @@ const search = (req, res) => {
                 day: dayFormatted
             };
         });
-        if (formattedResults.length  > 0) {
+        if (formattedResults.length > 0) {
             return res.render('doctoraccount.hbs', {
-                appointments: formattedResults, 
+                appointments: formattedResults,
                 doctorname: req.session.doctorname,
                 doctorProfileImage: req.session.doctorimage
             });
@@ -237,5 +189,266 @@ const search = (req, res) => {
         }
     });
 };
+ //multer config
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '..', 'public', 'uploads'));
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+        cb(null, file.originalname);
+
+    }
+});
+
+const upload = multer({ storage: storage });
+// edit profile
+router.post('/doctoreditprofile', upload.single('fileToUpload'), async (req, res) => {
+    const { name, password, cpassword } = req.body;
+    const ID = req.session.doctorid;
+
+    const file = req.file;
+    console.log(req.file)
+
+    if (password !== cpassword) {
+        return res.render('doctoreditprofile', {
+            message: "Passwords do not match."
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    try {
+        const userProfileImagePath = path.join('/uploads', req.file.originalname);
+        let filepath = file ? file.path : null;
+        if (filepath) {
+            await db.promise().query('UPDATE doctor SET name = ?, password = ?, image = ? WHERE ID = ?', [name, hashedPassword, userProfileImagePath, ID]);
+
+        }
+
+        const [results] = await db.promise().query('SELECT name, image FROM doctor WHERE ID = ?', [ID]);
+        const { name: doctorname, image: doctorProfileImage } = results[0];
+
+
+        return res.render('doctoreditprofile', {
+            message: "Profile updated successfully!",
+            doctorname: doctorname,
+            doctorProfileImage: doctorProfileImage
+        });
+
+    } catch (err) {
+        console.error(err);
+        return res.render('doctoreditprofile', { message: "An error occurred" });
+    }
+
+});
+// add document + blockhain 
+
+
+// New configuration for blockchain document uploads
+const blockchainStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+const blockchainUpload = multer({ storage: blockchainStorage });
+const mnemonic = 'render must plunge suggest eight motion bitter agent square crater addict expect';
+const infuraUrl = 'https://sepolia.infura.io/v3/b2b1ddd3656a46e5a80d311155018d4e';
+
+const provider = new HDWalletProvider({
+    mnemonic: {
+        phrase: mnemonic
+    },
+    providerOrUrl: infuraUrl,
+});
+
+const web3 = new Web3(provider);
+const ipfsClient = create({ host: '127.0.0.1', port: '5001', protocol: 'http' });
+const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "fileName",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "ipfsHash",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "doctorId",
+                "type": "uint256"
+            }
+        ],
+        "name": "upload",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "doctorId",
+                "type": "uint256"
+            }
+        ],
+        "name": "getDoctorDocuments",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "string",
+                        "name": "fileName",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "ipfsHash",
+                        "type": "string"
+                    }
+                ],
+                "internalType": "struct StockageHachages.Document[]",
+                "name": "",
+                "type": "tuple[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "doctorId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "index",
+                "type": "uint256"
+            }
+        ],
+        "name": "getDocumentDetails",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+router.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+        // Check if a file was uploaded
+        if (!req.file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+
+        // Read the uploaded file
+        const fileData = await fs.readFile(req.file.path);
+        console.log('File data:', fileData.toString()); // Log the file content
+
+        // Add the file to IPFS
+        const ipfsResult = await ipfsClient.add(fileData);
+        console.log('IPFS Result:', ipfsResult); // Log the IPFS add result
+        const ipfsHash = ipfsResult.cid.toString();
+
+        // Delete the temporary file
+        await fs.unlink(req.file.path);
+
+        console.log('Doctor ID from session:', req.session.doctorid); // Log the doctor ID from session
+
+        console.log('IPFS Hash:', ipfsHash);
+        if (!ipfsHash) {
+            return res.status(500).json({ message: 'Error uploading file: IPFS hash is undefined' });
+        }
+
+        // Retrieve the doctor ID from the session
+        const doctorId = req.session.doctorid;
+
+        // Retrieve the file name from the request
+        const fileName = req.body.fileName;
+
+        // Set the gas limit and get the current gas price
+        const gasLimit = 5500000; // Set the gas limit according to your requirements
+        const gasPrice = await web3.eth.getGasPrice(); // Get the current gas price
+
+        // Construct the transaction object
+        const txObject = {
+            from: '0xcb0D87301033298553023673d60070Ee65FD1252',
+            to: '0xd9145CCE52D386f254917e481eB44e9943F39138',
+            gas: gasLimit, // Set the gas limit here
+            gasPrice: gasPrice, // Set the gas price here
+            data: contract.methods.upload(fileName, ipfsHash, doctorId).encodeABI()
+        };
+
+        // Sign the transaction
+        const privateKey = '0e326c1665a619ef71900df6fecdff6dcb27437028e3c28ebcbed344a6dfbba4';
+        const signedTx = await web3.eth.accounts.signTransaction(txObject, privateKey);
+
+        // Send the signed transaction
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        console.log('Transaction receipt:', receipt);
+
+        res.redirect('/document');
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        res.status(500).json({ message: 'Error uploading file' });
+    }
+});
+router.get('/documents', async (req, res) => {
+    try {
+        // Vérifier si l'ID du médecin est présent dans la session et s'il est valide
+        const doctorId = req.session.doctorId;
+        if (!doctorId || doctorId <= 0) {
+            return res.status(400).json({ message: 'Invalid doctor ID' });
+        }
+
+        // Appeler la fonction de vue du contrat pour récupérer les documents du médecin spécifique
+        const contractResponse = await contract.methods.getDoctorDocuments(doctorId).call();
+
+        // Vérifier si des documents ont été renvoyés
+        if (!contractResponse || contractResponse.length === 0) {
+            return res.status(404).json({ message: 'No documents found for the doctor' });
+        }
+
+        // Mapper les données renvoyées par le contrat pour les documents
+        const documents = contractResponse.map(doc => ({
+            fileName: doc.fileName,
+            ipfsHash: doc.ipfsHash
+        }));
+
+        // Renvoyer les documents en réponse à la requête HTTP
+        res.json({ documents });
+    } catch (error) {
+        console.error('Error fetching documents:', error);
+        // Gérer les erreurs spécifiques et renvoyer des messages d'erreur descriptifs
+        if (error.code === 4001) {
+            return res.status(401).json({ message: 'Unauthorized access: Please log in' });
+        } else {
+            return res.status(500).json({ message: 'Error fetching documents from blockchain' });
+        }
+    }
+});
 
 module.exports = { signin, upload, forget, reset, search, router };
+//module.exports = ipfs;
